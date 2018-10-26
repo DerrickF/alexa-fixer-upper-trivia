b82363d6c727d71f7f6f8b524e6a9b7163e9d089
Derrick Fink <DerrickFink@boisestate.edu>
2018-10-26 12:11:40 -0600

    init repo

---
+++ question.js
@@ -0,0 +1,157 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_GB: [ ],
+    QUESTIONS_EN_US: [
+        {
+            'What year did Magnolia Market open?': [
+                '2003',
+                '1999',
+                '2007',
+                '2012',
+                '1985',
+                '2001',
+            ],
+        },
+        {
+            'How many siblings does Joanna Gains have?': [
+                '2 Sisters',
+                '1 Sister and 1 Brother',
+                '2 Brothers and 2 Sisters',
+                '0, She is an only child',
+                '1 Sister',
+                '1 Brother',
+                '3 Brothers',
+            ],
+        },
+        {
+            'What is Chip\'s real first name?' : [
+                'Chip',
+                'Christopher',
+                'Carter',
+                'Chester',
+                'Clinton'
+            ],
+        },
+        {
+            'What state was Chip born in?': [
+                'New Mexico',
+                'Arazona',
+                'Texas',
+                'Idaho',
+                'California'
+                
+            ],
+        },
+        {
+            'What state was Joanna born in?': [
+                'Kansas',
+                'Texas',
+                'Ohio',
+                'Alabama',
+            ],
+        },
+        {
+            'What major appliance does the Jains family not have in their farm house?': [
+                'Television',
+                'Refridgeator',
+                'Coffee Maker',
+                'Dishwasher',
+                'Air Conditioning'
+            ],
+        },
+        {
+            'What did chip want to be when he was growing up?': [
+                'Baseball Player',
+                'Police Officer',
+                'Skate Border',
+                'Fire Fighter',
+            ],
+        },
+        {
+            'In what town does the Fixer Upper show take place?': [
+                'Waco, Texas',
+                'Weatherford, Texas',
+                'Westland, Texas',
+                'Austin, Texas',
+            ],
+        },
+        {
+            'How did Chip and Joanna meet?': [
+                'In the auto shop her dad owned when he went to have his brakes fixed.',
+                'In an impound lot. Jo\'s father owned after his car was towed',
+                'In a flea market Joanna and her dad would go to every Sunday.',
+                'Shopping for furnature at an antique store',
+            ],
+        },
+        {
+            'How many kids to Chip and Joanna have?': [
+                'Five',
+                'One',
+                'Four',
+                'Three',
+                'Two'
+            ],
+        },
+        {
+            'What university did both Chip and Joanna attend?': [
+                'Baylor University',
+                'Texas A and M',
+                'University of Texas as Austin',
+                'Texas Tech',
+            ],
+        },
+        {
+            'Chip and Joanna\'s business is named after what kind of plant?': [
+                'Magnolia',
+                'Hydrangea',
+                'Orchird',
+                'Lilac',
+            ],
+        },
+        {
+            'What was the title of Chip and Joanna\'s book?': [
+                'The Magnolia Story',
+                'Fixer Upper',
+                'Capital Gains',
+                'Chip and Joanna\'s Magnolia Story',
+            ],
+        },
+        {
+            'What was Joanna\'s television debut?': [
+                'A automotive repair shop commercial',
+                'Fixer Upper',
+                'Local talk show host',
+                'Local NBC correspondent for NBC news',
+            ],
+        },
+        {
+            'What type of home did they remodel in the shows pre piolt sizzle reel?': [
+                'House Boat',
+                'Single story ranch',
+                'Mid centry modern 2 story',
+                'Victorian 3 bedroom',
+            ],
+        },
+        {
+            'Has Chip ever been in Jail?': [
+                'Yes',
+                'No',
+            ],
+        },
+        {
+            'What was Joanna\'s first job?': [
+                'Working at an all-you-can-eat buffett',
+                'Working in her father\'s auto repair shop' ,
+                'Flipping house',
+                'Interior designer',
+            ],
+        },
+        
+    ],
+  
+};
